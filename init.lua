2bf0ca84cf25730f73886c3d3bdf0baaf47e9d63
warmek <w4rmek@proton.me>
2025-07-27 15:07:08 +0200

    Disable text wraping

--- init.lua
+++ init.lua
@@ -18,6 +18,9 @@ vim.opt.number = true
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
 
+
+vim.opt.wrap = false
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
