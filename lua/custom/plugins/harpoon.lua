8b9c2dc2d888824fd3445f590e5cf98cd751bc9f
Bartosz Stolarczyk <bars@netcompany.com>
2025-08-18 15:05:19 +0200

    Echo harpoon

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -11,7 +11,7 @@ return {
 
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
-    end)
+      vim.api.nvim_echo({{'File added!', 'None'}}, false, {})    end)
     vim.keymap.set('n', '<C-e>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
