b537ac626af17b1b8e47eb2a3012677d3cb00b22
warmek <w4rmek@proton.me>
2025-10-14 20:01:01 +0200

    Install quickscope

--- init.lua
+++ init.lua
@@ -1021,6 +1021,7 @@ require('lazy').setup({
       -- your configuration comes here; leave empty for default settings
     },
   },
+  {'unblevable/quick-scope'},
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
